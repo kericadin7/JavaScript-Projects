008a9873b737dcedbab9db5ce50bd887d1351378
Adin <kericadin7@gmail.com>
2026-04-18 12:28:54 +0200

    happy birthday demo version
    pokusaj cestitke za rodjendan, malo failalo

---
+++ hepi brzdej/js/script.js
@@ -0,0 +1,38 @@
+const text = "Ti si moj cuker i ja tebe volim najvišeee 🤍";
+let i = 0;
+
+function typeWriter() {
+  if (document.getElementById("typing") && i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 50);
+  }
+}
+
+typeWriter();
+
+function showReasons() {
+  const reasons = [
+    "Volim te jer si ti moj cuker 🤍",
+    "Volim te jer me nasmijavaš 🤍",
+    "Volim te jer si predivna osoba 🤍",
+    "VOLIM TE ZATO ŠTO SI TI MOJA LUDICA 🤍"
+  ];
+
+  const container = document.getElementById("reasons");
+
+  reasons.forEach((r, index) => {
+    setTimeout(() => {
+      const div = document.createElement("div");
+      div.className = "box";
+      div.innerText = r;
+      container.appendChild(div);
+    }, index * 400);
+  });
+
+  document.getElementById("nextBtn").classList.remove("hidden");
+}
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
